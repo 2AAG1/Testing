84a6c09f522bd3d03028a97d7a7ee6c38853fd54
Antriksh Goyal <antrikshgoyal@Antrikshs-MacBook-Air.local>
2024-10-31 13:42:17 +0530

    Harvard lecture practise

--- Git_R-studio_Test.R
+++ Git_R-studio_Test.R
@@ -1,3 +1,25 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+library(rafalib)
+library(swirl)
+
+v <-  c(2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23)
+
+mean(v)
+average(v)
+
+View(cars)
+
+class(cars)
+
+x <- c(1:25)
+head(x)
+
+class(x)
+
+y <- numeric(length(x))
+
+for (i in 1:length(x)) { y[i] <- x[i] ** 2}
+ sum(y)
